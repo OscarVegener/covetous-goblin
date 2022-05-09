a66543e9094fc28b937fbf5bba388d7c8a9a2c2b
Bohdan Yaroshchuk <49143627+OscarVegener@users.noreply.github.com>
2022-05-09 23:25:48 +0300

    Add base navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+            <Logo className="logo"/>
+        </Link>
+        <Link to="/sign-in">
+            SignIn
+        </Link>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
