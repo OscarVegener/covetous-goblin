2ad7532b9e57db142aacb95f9b7f4654eaabfe5a
Bohdan Yaroshchuk <49143627+OscarVegener@users.noreply.github.com>
2022-05-15 20:35:48 +0300

    Add firebase; add signin component

--- src/routes/signIn/signIn.component.jsx
+++ src/routes/signIn/signIn.component.jsx
@@ -1,6 +1,22 @@
+import { signInWithGooglePopup, createUserDoc } from "../../utils/firebase/firebase.utils"; 
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        signInWithGooglePopup().then(
+            response => createUserDoc(response.user)
+        )
+        .then(
+            userRef => console.log(userRef)
+        )
+        .catch(error => {
+            console.log(`Error happened: ${error}`);
+        });
+    }
+
+
     return (
         <div>
+            <button onClick={logGoogleUser}>Sign in with Google</button>
             <h1>SignIn</h1>
         </div>
     )
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDL6NWiScHAuad5_csIWpdcP3NGK6foj18",
+  authDomain: "covetousgoblin.firebaseapp.com",
+  projectId: "covetousgoblin",
+  storageBucket: "covetousgoblin.appspot.com",
+  messagingSenderId: "142108000084",
+  appId: "1:142108000084:web:f8b196ff1339be426712c1"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+export const db = getFirestore();
+
+
+export const createUserDoc = async (user) => {
+  const userRef = await doc(db, "users", user.uid);
+  const userSnapshot = await getDoc(userRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = user;
+    const createdAt = new Date();
+    setDoc(userRef, { displayName, email, createdAt }).catch(
+      error => console.log(`Error creating user: ${error}`)
+    );
+  }
+  return userRef;
+}
